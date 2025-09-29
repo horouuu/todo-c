360d0f2eeceb1224712a04f8b97a172c5def7a80
horouuu <fernandafadja@gmail.com>
2025-09-29 20:40:22 +0800

    refactor: adapted to fit ansi standards

--- writer/writer.c
+++ writer/writer.c
@@ -7,18 +7,30 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+void create_todo_directory()
+{
+#ifdef _WIN32
+    _mkdir(".todo");
+#else
+    mkdir(".todo", 0755);
+#endif
+}
+
 int create_todo(const char *todo_message, const unsigned int priority, const unsigned int id, const time_t timestamp, char **out)
 {
-
-    int len = 32;
-    int curr_len = 0;
     size_t out_size;
-    char timestamp_string[len];
-    char priority_string[len];
-    char id_string[len];
-    snprintf(timestamp_string, len, "%" PRIdMAX, (intmax_t)timestamp);
-    snprintf(priority_string, len, "%u", priority);
-    snprintf(id_string, len, "%u", id);
+    char timestamp_string[32];
+    char priority_string[32];
+    char id_string[32];
+
+    if (strlen(todo_message) > 60)
+    {
+        return -1;
+    }
+
+    sprintf(timestamp_string, "%" PRIdMAX, (intmax_t)timestamp);
+    sprintf(priority_string, "%u", priority);
+    sprintf(id_string, "%u", id);
 
     out_size = strlen(timestamp_string) + strlen(todo_message) + strlen(priority_string) + strlen(id_string) + 4 + 1 + 1;
     *out = malloc(out_size);
@@ -28,7 +40,7 @@ int create_todo(const char *todo_message, const unsigned int priority, const uns
         return 1;
     }
 
-    snprintf(*out, out_size, "%s,\"%s\",%s,%s,0", timestamp_string, todo_message, priority_string, id_string);
+    sprintf(*out, "%s,%s,\"%s\",%s,0", priority_string, timestamp_string, todo_message, id_string);
     return 0;
 }
 
@@ -45,13 +57,7 @@ int todo_add(const char *file_name, const char *todo_message, const unsigned int
     fptr = fopen(file_name, "r+");
     if (!fptr)
     {
-
-#ifdef _WIN32
-        _mkdir(".todo");
-#else
-        mkdir(".todo", 0755);
-#endif
-
+        create_todo_directory();
         fptr = fopen(file_name, "w+");
         if (!fptr)
             return 1;
@@ -73,6 +79,12 @@ int todo_add(const char *file_name, const char *todo_message, const unsigned int
     if (ctstatus != 0)
     {
         printf("Error.\n");
+        return 1;
+    }
+    else if (ctstatus == -1)
+    {
+        printf("Error: todo message too long. Maximum: 60 characters.");
+        return 1;
     }
     else
     {

756b252a79b0bbf94073b7082ac1d7bf39271ecf
horouuu <fernandafadja@gmail.com>
2025-09-29 22:56:24 +0800

    feat: code refactors
    abstract file/dir create into their own functions, replaced VLAs, changed function signatures to include str version of priority

--- writer/writer.c
+++ writer/writer.c
@@ -6,8 +6,9 @@
 #include <direct.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <ctype.h>
 
-void create_todo_directory()
+void todo_create_directory()
 {
 #ifdef _WIN32
     _mkdir(".todo");
@@ -16,12 +17,25 @@ void create_todo_directory()
 #endif
 }
 
-int create_todo(const char *todo_message, const unsigned int priority, const unsigned int id, const time_t timestamp, char **out)
+int todo_create_file(const char *file_name, FILE *fptr)
+{
+    fptr = fopen(file_name, "w+");
+    if (!fptr)
+    {
+        return 1;
+    }
+    fprintf(fptr, "%-12u\n", 1);
+    fflush(fptr);
+    rewind(fptr);
+    printf("\n");
+
+    return 0;
+}
+
+int todo_create(const char *todo_message, const char *priority, const char *id, const time_t timestamp, char **out)
 {
     size_t out_size;
     char timestamp_string[32];
-    char priority_string[32];
-    char id_string[32];
 
     if (strlen(todo_message) > 60)
     {
@@ -29,53 +43,67 @@ int create_todo(const char *todo_message, const unsigned int priority, const uns
     }
 
     sprintf(timestamp_string, "%" PRIdMAX, (intmax_t)timestamp);
-    sprintf(priority_string, "%u", priority);
-    sprintf(id_string, "%u", id);
 
-    out_size = strlen(timestamp_string) + strlen(todo_message) + strlen(priority_string) + strlen(id_string) + 4 + 1 + 1;
+    out_size = strlen(timestamp_string) + strlen(todo_message) + strlen(priority) + strlen(id) + 4 + 1 + 1;
     *out = malloc(out_size);
 
-    if (!out)
+    if (!*out)
     {
         return 1;
     }
 
-    sprintf(*out, "%s,%s,\"%s\",%s,0", priority_string, timestamp_string, todo_message, id_string);
+    sprintf(*out, "%s,%s,\"%s\",%s,0", id, timestamp_string, todo_message, priority);
     return 0;
 }
 
-int todo_add(const char *file_name, const char *todo_message, const unsigned int priority)
+int todo_add(const char *file_name, const char *todo_message, const char *priority)
 {
     FILE *fptr;
     time_t timestamp;
-    char curr_id_raw[12];
+    char *curr_id_raw = malloc(13);
+    char *curr_id = malloc(12);
+    char *curr_id_ptr = curr_id;
     unsigned int curr_available_id;
     int ctstatus;
+    int cfstatus;
     char *todo_string;
     char *fgets_result;
 
     fptr = fopen(file_name, "r+");
     if (!fptr)
     {
-        create_todo_directory();
-        fptr = fopen(file_name, "w+");
-        if (!fptr)
+        todo_create_directory();
+        cfstatus = todo_create_file(file_name, fptr);
+        if (cfstatus != 0)
+        {
+            printf("Error creating file.");
             return 1;
-        fprintf(fptr, "0\n");
-        fflush(fptr);
-        printf("\n");
+        }
     }
 
-    rewind(fptr);
     fgets_result = fgets(curr_id_raw, sizeof(curr_id_raw), fptr);
     if (!fgets_result)
     {
-        strcpy(curr_id_raw, "0");
+        strcpy(curr_id_raw, "1");
     }
 
     time(&timestamp);
-    curr_available_id = strtol(curr_id_raw, NULL, 0);
-    ctstatus = create_todo(todo_message, priority, curr_available_id, timestamp, &todo_string);
+    while (!isspace(*curr_id_raw) && *curr_id_raw != '\0')
+    {
+        *curr_id = *curr_id_raw;
+        curr_id_ptr++;
+        curr_id_raw++;
+    }
+    *curr_id_ptr = '\0';
+
+    curr_available_id = strtol(curr_id, NULL, 0);
+    if (!curr_available_id)
+    {
+        printf("Error reading ID: either non-integer or reserved value 0 found in file.\n");
+        return 1;
+    }
+
+    ctstatus = todo_create(todo_message, priority, curr_id, timestamp, &todo_string);
     if (ctstatus != 0)
     {
         printf("Error.\n");

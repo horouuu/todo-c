fa6cbeeb0fcfecf41637b5e9eb28df05c3767e36
horouuu <fernandafadja@gmail.com>
2025-09-29 22:54:58 +0800

    feat: add new func headers

--- writer/writer.h
+++ writer/writer.h
@@ -2,7 +2,10 @@
 #define WRITER_H
 #include <time.h>
 
-int todo_add(const char *file_name, const char *todo_message, const unsigned int priority);
-int create_todo(const char *todo_message, const unsigned int priority, const unsigned int id, const time_t timestamp, char **out);
+int todo_add(const char *file_name, const char *todo_message, const char *priority);
+int todo_create(const char *todo_message, const char *priority, const char *id, const time_t timestamp, char **out);
+int todo_delete(const char *file_name, const char id);
+void todo_create_directory();
+void todo_create_file(const char *file_name, FILE *fptr);
 
 #endif

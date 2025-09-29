090acd34a2db6b026cb6ccde61d7b5541839234e
horouuu <fernandafadja@gmail.com>
2025-09-29 23:32:07 +0800

    fix: fix writer function signatures

--- writer/writer.h
+++ writer/writer.h
@@ -4,8 +4,8 @@
 
 int todo_add(const char *file_name, const char *todo_message, const char *priority);
 int todo_create(const char *todo_message, const char *priority, const char *id, const time_t timestamp, char **out);
-int todo_delete(const char *file_name, const char id);
+int todo_delete(const char *file_name, const char *id);
 void todo_create_directory();
-void todo_create_file(const char *file_name, FILE *fptr);
+void todo_create_file(const char *file_name, FILE **fptr);
 
 #endif
